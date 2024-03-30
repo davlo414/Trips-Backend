4a73ddf131bf9a683bd664d87b0edef7e2286d7d
davlo414 <davlo414@gmail.com>
2024-03-30 02:25:52 -0500

    updates

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 from trips.models import Trip
+from django.db.models import Q
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.StringRelatedField(source="user")
@@ -13,4 +14,4 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def get_shared_trips(self, obj):
         current_user = self.context.get('request').user
-        return Trip.objects.filter(people__in=[current_user, obj.user]).count()
+        return Trip.objects.filter(people=current_user).filter(people=obj.user).count()
--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,6 @@ from django.db.models import Count
 from django.db.models import Q
 
 class UserViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all().order_by('user__username')
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        current_user = self.request.user
-        return Profile.objects.annotate(
-            shared_trips_count=Count('user__trip', filter=Q(user__trip__people=current_user))
-        ).exclude(user=current_user).order_by('-shared_trips_count')
