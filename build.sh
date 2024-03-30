69dd03ff4f8294e71d5d7454eff5250a0d90be1b
davlo414 <davlo414@gmail.com>
2024-03-29 22:47:19 -0500

    added superuser creation

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ $(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).count())") -eq 0 ]; then
+    python manage.py createsuperuser --noinput --username admin --email admin@example.com
+fi
