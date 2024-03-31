818c3949292cd4b87d69f09a041d0556c794b7c0
davlo414 <davlo414@gmail.com>
2024-03-31 07:40:59 -0500

    added import

--- trips_project/prod_settings.py
+++ trips_project/prod_settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from google.oauth2 import service_account
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
